4a6bc7fc3287dcfb82bc3cff0e8ec5818e73d778
TeamDman <TeamDman9201@gmail.com>
2019-12-28 21:30:29 -0500

    Update gradle for dependencies

--- src/MainForm.java
+++ src/MainForm.java
@@ -1,8 +1,11 @@
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumn;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class MainForm {
 	private JButton    btnBrowseOutput;
@@ -10,11 +13,31 @@ public class MainForm {
 	private JPanel     panelMain;
 	private JPanel     panelOutput;
 	private JTextField txtOutputPath;
+	private JTable     tblFiles;
+	private JPanel panelInput;
+	private ArrayList<File> inputFiles = new ArrayList<>();
 
 	public MainForm() {
+		panelInput.setBorder(BorderFactory.createTitledBorder(panelInput.getName()));
 		panelOutput.setBorder(BorderFactory.createTitledBorder(panelOutput.getName()));
 		btnBrowseOutput.addMouseListener(new BrowseFilesAdapter());
 		btnOpenOutputDir.addMouseListener(new OpenOutputAdapter());
+		new FileDrop(panelMain, files -> {
+			for (File f : files)
+				addFile(f);
+			draw();
+		});
+	}
+
+	public void addFile(File file) {
+		inputFiles.add(file);
+	}
+
+	public void draw() {
+		DefaultTableModel model = (DefaultTableModel) tblFiles.getModel();
+		for (File f : inputFiles) {
+			model.addRow(new Object[]{f.getAbsolutePath(),"idk","lmao"});
+		}
 	}
 
 	public static void main(String[] args) {
@@ -29,6 +52,10 @@ public class MainForm {
 		instance.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		instance.pack();
 		instance.setVisible(true);
+	}
+
+	private void createUIComponents() {
+		tblFiles = new JTable(new DefaultTableModel(new Object[]{"Input File", "Input Time", "Output Time"},0));
 	}
 
 
