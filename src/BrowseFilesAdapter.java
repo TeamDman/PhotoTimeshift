007ace5b22aae28f27d1823e12feb67c385e63c5
TeamDman <TeamDman9201@gmail.com>
2019-12-28 22:08:06 -0500

    Remove old adapter class

--- src/BrowseFilesAdapter.java
+++
@@ -1,8 +0,0 @@
-import javax.swing.*;
-import javax.swing.filechooser.FileFilter;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.io.File;
-import java.util.function.Consumer;
-
-
